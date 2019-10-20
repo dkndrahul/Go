113b207be3064951cd8620c9d52481fc8c729b58
dkndrahul <dkndrahul@live.com>
2019-10-20 16:07:32 +0530

    added save to file function which returns a string

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,7 @@
 package main
 
 func main() {
-	// var card string = "Ace of spades"
-	// or
-	// card := "Ace of spades" // 100 % equivalent to the above declaration
-	// card = "Five of Diamonds"
-	// fmt.Println(card)
-	cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards := newDeck()
+	cards.saveTofile("my_cards")
 }
