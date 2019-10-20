da88faa15a9ff58c1d6a555ee47f124d1d711984
dkndrahul <dkndrahul@live.com>
2019-10-20 11:48:29 +0530

    deck and deal functions added

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of spades"
 	// or
-	card := "Ace of spades" // 100 % equivalent to the above declaration
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	// card := "Ace of spades" // 100 % equivalent to the above declaration
+	// card = "Five of Diamonds"
+	// fmt.Println(card)
+	cards := newDeck()
+
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	remainingCards.print()
 }
