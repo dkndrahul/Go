7dc43e85156c76027270c0e302409af48e201e57
dkndrahul <dkndrahul@live.com>
2019-10-20 11:10:16 +0530

    Go

---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of spades"
+	// or
+	card := "Ace of spades" // 100 % equivalent to the above declaration
+	card = "Five of Diamonds"
+	fmt.Println(card)
+}
