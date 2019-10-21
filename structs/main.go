22505ef5f56b8f10ec8651c393f31b623d0093fc
dkndrahul <dkndrahul@live.com>
2019-10-21 18:52:11 +0530

    creating embeded structs

--- structs/main.go
+++ structs/main.go
@@ -1,1 +1,37 @@
 package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	// rahul :=  person{"Rahul", "Deekonda"} one way of delcaring
+	// rahul := person{firstName: "Rahul", lastName: "Deekonda"} // other way of declaring with property name like key value pair
+
+	// var rahul person
+
+	// rahul.firstName = "Rahul"
+	// rahul.lastName = "Deekonda"
+	// fmt.Printf("%+v", rahul)
+	// fmt.Println(rahul)
+
+	rahul := person{
+		firstName: "Rahul",
+		lastName:  "Deekonda",
+		contact: contactInfo{
+			email:   "dkndrahul@gmail.com",
+			zipcode: 503001,
+		},
+	}
+
+	fmt.Printf("%+v", rahul)
+}
