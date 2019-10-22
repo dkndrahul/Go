e5bcec005ae4abd88e0d1bf25e817c194e1ea273
dkndrahul <dkndrahul@live.com>
2019-10-22 18:22:23 +0530

    ways of creating maps

--- map/main.go
+++ map/main.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#00000",
-		"green": "#4785847",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#00000",
+	// 	"green": "#4785847",
+	// }
+	// var  colors map[string]string // the other way of initializing the map
+
+	colors := make(map[string]string) // The other way of cretaing maps
+	colors["RED"] = "jkjahfa"
 
 	fmt.Println(colors)
 }
